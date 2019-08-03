861db81866a1e20c74f78594aa67e57ab0847922
Zachary Kuhn <zachary@zacharykuhn.com>
2019-08-03 08:31:27 -0400

    Add remove item

--- src/items.rs
+++ src/items.rs
@@ -3,6 +3,7 @@ use serde::*;
 
 trait Items {
     fn get_item(&self, access_token: &str) -> Result<GetItemResponse, Error>;
+    fn remove_item(&self, access_token: &str) -> Result<RemoveItemResponse, Error>;
 }
 
 #[derive(Deserialize)]
@@ -28,6 +29,19 @@ pub struct GetItemResponse {
     item: Item,
 }
 
+#[derive(Serialize)]
+struct RemoveItemRequest<'a> {
+    client_id: &'a str,
+    secret: &'a str,
+    access_token: &'a str,
+}
+
+#[derive(Deserialize)]
+pub struct RemoveItemResponse {
+    response_id: String,
+    removed: bool,
+}
+
 impl<'a> Items for Client<'a> {
     fn get_item(&self, access_token: &str) -> Result<GetItemResponse, Error> {
         if access_token == "" {
@@ -43,5 +57,21 @@ impl<'a> Items for Client<'a> {
         serde_json::to_string(&req)
             .map_err(|err| Error::new(Kind::Json(err)))
             .and_then(|json_body| self.call("/item/get", &json_body))
+    }
+
+    fn remove_item(&self, access_token: &str) -> Result<RemoveItemResponse, Error> {
+        if access_token == "" {
+            return Err(Error::new(Kind::EmptyToken));
+        }
+
+        let req = RemoveItemRequest {
+            client_id: self.client_id,
+            secret: self.secret,
+            access_token,
+        };
+
+        serde_json::to_string(&req)
+            .map_err(|err| Error::new(Kind::Json(err)))
+            .and_then(|json_body| self.call("/item/remove", &json_body))
     }
 }
