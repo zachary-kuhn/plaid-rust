d7785b198f68d2ea08158d86ec6f050c634a5d5f
Zachary Kuhn <zachary@zacharykuhn.com>
2019-08-05 20:06:16 -0400

    Add create stripe token

--- src/processors.rs
+++ src/processors.rs
@@ -12,6 +12,11 @@ trait Processors {
         access_token: &str,
         account_id: &str,
     ) -> Result<CreateDwollaTokenResponse, Error>;
+    fn create_stripe_token(
+        &self,
+        access_token: &str,
+        account_id: &str,
+    ) -> Result<CreateStripeTokenResponse, Error>;
 }
 
 #[derive(Serialize)]
@@ -40,6 +45,20 @@ struct CreateDwollaTokenRequest<'a> {
 pub struct CreateDwollaTokenResponse {
     response_id: String,
     processor_token: String,
+}
+
+#[derive(Serialize)]
+struct CreateStripeTokenRequest<'a> {
+    client_id: &'a str,
+    secret: &'a str,
+    access_token: &'a str,
+    account_id: &'a str,
+}
+
+#[derive(Deserialize)]
+pub struct CreateStripeTokenResponse {
+    response_id: String,
+    stripe_bank_account_token: String,
 }
 
 impl<'a> Processors for Client<'a> {
@@ -87,5 +106,30 @@ impl<'a> Processors for Client<'a> {
         serde_json::to_string(&req)
             .map_err(|err| Error::new(Kind::Json(err)))
             .and_then(|json_body| self.call("/processor/dwolla/processor_token/create", &json_body))
+    }
+
+    fn create_stripe_token(
+        &self,
+        access_token: &str,
+        account_id: &str,
+    ) -> Result<CreateStripeTokenResponse, Error> {
+        if access_token == "" || account_id == "" {
+            Err(Error::new(Kind::ValidationError(
+                "access token and account ID must be specified",
+            )))?
+        }
+
+        let req = CreateStripeTokenRequest {
+            client_id: self.client_id,
+            secret: self.secret,
+            access_token,
+            account_id,
+        };
+
+        serde_json::to_string(&req)
+            .map_err(|err| Error::new(Kind::Json(err)))
+            .and_then(|json_body| {
+                self.call("/processor/stripe/bank_account_token/create", &json_body)
+            })
     }
 }
