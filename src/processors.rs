6277026482117975924d0845481cedb13d045cfc
Zachary Kuhn <zachary@zacharykuhn.com>
2019-08-05 20:03:02 -0400

    Add create dwolla token

--- src/processors.rs
+++ src/processors.rs
@@ -7,6 +7,11 @@ trait Processors {
         access_token: &str,
         account_id: &str,
     ) -> Result<CreateApexTokenResponse, Error>;
+    fn create_dwolla_token(
+        &self,
+        access_token: &str,
+        account_id: &str,
+    ) -> Result<CreateDwollaTokenResponse, Error>;
 }
 
 #[derive(Serialize)]
@@ -19,6 +24,20 @@ struct CreateApexTokenRequest<'a> {
 
 #[derive(Deserialize)]
 pub struct CreateApexTokenResponse {
+    response_id: String,
+    processor_token: String,
+}
+
+#[derive(Serialize)]
+struct CreateDwollaTokenRequest<'a> {
+    client_id: &'a str,
+    secret: &'a str,
+    access_token: &'a str,
+    account_id: &'a str,
+}
+
+#[derive(Deserialize)]
+pub struct CreateDwollaTokenResponse {
     response_id: String,
     processor_token: String,
 }
@@ -45,5 +64,28 @@ impl<'a> Processors for Client<'a> {
         serde_json::to_string(&req)
             .map_err(|err| Error::new(Kind::Json(err)))
             .and_then(|json_body| self.call("/processor/apex/processor_token/create", &json_body))
+    }
+
+    fn create_dwolla_token(
+        &self,
+        access_token: &str,
+        account_id: &str,
+    ) -> Result<CreateDwollaTokenResponse, Error> {
+        if access_token == "" || account_id == "" {
+            Err(Error::new(Kind::ValidationError(
+                "access token and account ID must be specified",
+            )))?
+        }
+
+        let req = CreateDwollaTokenRequest {
+            client_id: self.client_id,
+            secret: self.secret,
+            access_token,
+            account_id,
+        };
+
+        serde_json::to_string(&req)
+            .map_err(|err| Error::new(Kind::Json(err)))
+            .and_then(|json_body| self.call("/processor/dwolla/processor_token/create", &json_body))
     }
 }
